f075a9927f7550e882c3076ca60808665d9bd7a1
汤苗 <1084491297@qq.com>
2021-12-22 16:33:48 +0800

    docs: 添加swagger文档

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle('Pet-Mall')
+    .setDescription('Pet-Mall Server API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
+  
   await app.listen(3000);
+  
 }
 bootstrap();
